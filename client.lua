27f6786d5ee9609af5546307a51314ac6f70a414
tickly50 <tomasskala03@gmail.com>
2025-05-01 11:17:04 +0200

    Update client.lua

--- client.lua
+++ client.lua
@@ -1,6 +1,6 @@
 Citizen.CreateThread( function()
  while true do
-    Citizen.Wait(0)
-    RestorePlayerStamina(PlayerId(), 1.0)
+    Citizen.Wait(10000)
+    ResetPlayerStamina(PlayerId(), 1.0)
 	end
 end)
